1a687f6e21e05a3dc03ed5bf6090e35b67c777ec
yirano <yirano@protonmail.ch>
2020-04-07 15:17:04 -0400

    Refactor codes to use Context API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
+import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 import data from './data';
 
 // Components
@@ -8,27 +10,34 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = item => {
+    // add the given item to the cart
+    setCart([...cart, item])
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  return (
+    <div className="App">
+      <CartContext.Provider value={cart}>
+        <Navigation cart={cart} />
+      </CartContext.Provider>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+      {/* Routes */}
+      <ProductContext.Provider value={{ products, addItem }}>
+        <Route exact path="/">
+          <Products />
+        </Route>
+      </ProductContext.Provider>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+      <CartContext.Provider value={cart}>
+        <Route path="/cart">
+          <ShoppingCart />
+        </Route>
+      </CartContext.Provider>
+    </div>
+  );
 }
 
 export default App;
