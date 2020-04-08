b654c255380c4e9f50e7e1792cbfc09fc1710d7c
yirano <yirano@protonmail.ch>
2020-04-07 22:07:50 -0400

    Delete comments

--- src/App.js
+++ src/App.js
@@ -3,20 +3,10 @@ import { Route } from 'react-router-dom';
 import { ProductContext } from './contexts/ProductContext'
 import { CartContext } from './contexts/CartContext'
 import data from './data';
-
-// Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
-// const useStateWithLocalStorage = localStorageKey => {
-//   const [cart, setCart] = useState([] || JSON.stringify(localStorage.getItem(localStorageKey)));
-
-//   useEffect(() => {
-//     JSON.stringify(localStorage.setItem(localStorageKey, cart))
-//   }, [cart])
-//   return [cart, setCart]
-// }
 
 function App() {
   const [products] = useState(data);
@@ -39,7 +29,6 @@ function App() {
         <Navigation cart={cart} />
       </CartContext.Provider>
 
-      {/* Routes */}
       <ProductContext.Provider value={{ products, addItem }}>
         <Route exact path="/">
           <Products />
